4e937c3ccf14b4428d51308d7a4599a28368d1ea
yangwq7 <yangwq7@aisainfo.com>
2021-05-10 20:21:37 +0800

    添加vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,7 @@
-/*
- * @Author: your name
- * @Date: 2021-05-10 17:04:10
- * @LastEditTime: 2021-05-10 17:04:10
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \blog-vue3\src\store\index.ts
- */
+import { createStore } from 'vuex'
+
+export const store = createStore({
+  state(){
+    count: 1
+  }
+})
