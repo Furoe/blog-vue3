9fe9d1ed441696026e648522d3cbe5064194a95c
yangwq7 <yangwq7@aisainfo.com>
2021-05-10 18:00:15 +0800

    first update

---
+++ src/router/routes.ts
@@ -0,0 +1,10 @@
+import { RouteRecordRaw } from "vue-router"
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../view/Home.vue')
+  }
+]
+export default routes
---
+++ src/store/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-10 17:04:10
+ * @LastEditTime: 2021-05-10 17:04:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \blog-vue3\src\store\index.ts
+ */
